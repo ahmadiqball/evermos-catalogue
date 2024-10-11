644b998b61bf57fb1e4e960c87ef7276e6c115ff
ahmadiqball <ahmadiqbalshinichi@gmail.com>
2024-10-11 15:29:50 +0800

    feat: initialize cart store using pinia

---
+++ src/store/cart.ts
@@ -0,0 +1,53 @@
+import { defineStore } from 'pinia';
+import type { Product } from '~/typings/index.entity';
+
+interface CartProductItem {
+  data: Product;
+  quantity: number;
+}
+
+export const useCartStore = defineStore('cart', {
+  state: () => ({
+    products: [] as Array<CartProductItem>,
+  }),
+  actions: {
+    add(product: Product) {
+      const existing = this.getCartItemByID(product.id);
+
+      if (existing) {
+        existing.quantity++;
+      } else {
+        this.products.push({
+          data: product,
+          quantity: 1,
+        });
+      }
+    },
+    increase(id: string) {
+      const existing = this.getCartItemByID(id);
+
+      if (existing) {
+        existing.quantity++;
+      }
+    },
+    decrease(id: string) {
+      const existing = this.getCartItemByID(id);
+
+      if (existing) {
+        if (existing.quantity < 2) {
+          this.remove(id);
+        } else {
+          existing.quantity--;
+        }
+      }
+    },
+    remove(id: string) {
+      this.products = this.products.filter((product) => product.data.id !== id);
+    },
+  },
+  getters: {
+    getCartItemByID(state) {
+      return (id: string) => state.products.find((item) => item.data.id === id);
+    },
+  },
+});
