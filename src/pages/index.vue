faac8aaf436ba6fe7dd2bf522f737413d254044f
ahmadiqball <ahmadiqbalshinichi@gmail.com>
2024-10-12 00:17:42 +0800

    feat: add product pagination

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,10 +4,11 @@ import { useHeadSafe } from '#imports';
 import { ref } from 'vue';
 
 const searchInput = ref('');
+const displayedPage = ref(1);
 const { data } = await useFetch(`/api/products`, {
   query: {
     limit: 12,
-    page: 1,
+    page: displayedPage,
     search: searchInput,
   },
 });
@@ -33,6 +34,18 @@ useHeadSafe({
         :key="product.id"
         :product="product"
       />
+    </div>
+
+    <div class="product-pagination-wrapper">
+      <button
+        v-for="page in data?.totalPage"
+        :key="page"
+        class="product-pagination"
+        :class="{ 'product-pagination-active': displayedPage === page }"
+        @click="displayedPage = page"
+      >
+        {{ page }}
+      </button>
     </div>
   </section>
 </template>
@@ -64,9 +77,36 @@ useHeadSafe({
 
 .product-list {
   display: grid;
-  margin-top: 40px;
+  margin: 40px 0;
   grid-template-columns: repeat(4, 1fr);
   gap: 16px;
+}
+
+.product-pagination-wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 4px;
+  row-gap: 8px;
+  margin: 40px 0;
+}
+
+.product-pagination {
+  cursor: pointer;
+  padding: 4px;
+  transition: ease-in 200ms;
+  font-size: 14px;
+}
+
+.product-pagination-active {
+  background-color: var(--color-green-400);
+  color: white;
+}
+
+.product-pagination:hover {
+  background-color: var(--color-green-400);
+  color: white;
 }
 
 @media (max-width: 1024px) {
@@ -79,6 +119,10 @@ useHeadSafe({
   .product-list {
     gap: 8px;
     grid-template-columns: repeat(2, 1fr);
+  }
+
+  .product-pagination {
+    font-size: 12px;
   }
 }
 </style>
